1390a6e9b5d6bda1b55825e8baeae05245b3a14d
thomblr <thomas.1111@live.be>
2021-01-14 17:57:06 +0100

    Add User routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -7,8 +7,8 @@ let User = require('../models/user.model');
 *   - Get a user         (/i/:id)
 *   - Get user by name   (/u/:username)
 *   - Add a new user     (/add)
-*   - Remove a user      (/del)
-*   - ...
+*   - Remove a user      (/del/:id)
+*   - Update a user      (/update/:id)
 */
 
 /**
@@ -38,6 +38,45 @@ router.route('/u/:username').get((req, res) => {
     User.find({username: req.params.username})
       .then(user => res.json(user))
       .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+/**
+ * Add a new user
+ */
+router.route('/add').post((req, res) => {
+    const newUser = new User({
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+    });
+
+    newUser.save()
+      .then(() => res.json('New User added!'))
+      .catch(err => res.json('Error: ' + err));
+});
+
+
+/**
+ * Remove a user
+ */
+router.route('/del/:id').delete((req, res) => {
+  const _id = req.params.id;
+  User.deleteOne({_id: _id})
+    .then(() => res.json('User deleted!'))
+    .catch(err => res.json('Error: ' + err));
+});
+
+
+/**
+ * Update a user
+ */
+router.route('/update/:id').put((req, res) => {
+  const _id = req.params.id;
+
+  User.findByIdAndUpdate(_id, req.body)
+    .then(() => res.json('User updated!'))
+    .catch(err => res.json('Error: ' + err));
 });
 
 module.exports = router;
