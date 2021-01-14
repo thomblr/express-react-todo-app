054b9fa87b3f0c9253f1194e60652f81ced9f1ac
thomblr <thomas.1111@live.be>
2021-01-14 15:36:26 +0100

    Add new status route

--- api/routes/status.js
+++ api/routes/status.js
@@ -5,6 +5,8 @@ let Status = require('../models/status.model');
  *  STATUS API MANAGEMENT
  *  - Get all status (/)
  *  - Get status by Id (/:id)
+ *  - Add new status (/add)
+ *  - Remove a status (/del/:id)
  */
 
 
@@ -25,6 +27,24 @@ router.route('/:id').get((req, res) => {
     Status.findById(req.params.id)
       .then(item => res.json(item))
       .catch(err => res.status(400).json('Error: ' + err));
+})
+
+
+/**
+ * Add a new status
+ */
+router.route('/add').post((req, res) => {
+    const value = req.body.value;
+    const color = req.body.color;
+
+    const newStatus = new Status({
+        value,
+        color,
+    });
+
+    newStatus.save()
+      .then(() => res.json('Status added!'))
+      .catch(err => res.json('Error: ' + err));
 })
 
 module.exports = router;
